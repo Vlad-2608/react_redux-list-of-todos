0137376a811d3a2a70c06cabf7403428fa83da0d
unknown <vladhafarov@gmail.com>
2025-07-11 18:56:00 +0300

    solution

--- src/features/currentTodo.ts
+++ src/features/currentTodo.ts
@@ -1,10 +1,49 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { Todo } from '../types/Todo';
+import { User } from '../types/User';
 
-const initialState = null as Todo | null;
+export type CurrentTodoState = {
+  currentTodo: Todo | null;
+  currentUser: User | null;
+  isLoading: boolean;
+};
+
+const initialState: CurrentTodoState = {
+  currentTodo: null,
+  currentUser: null,
+  isLoading: true,
+};
 
 export const currentTodoSlice = createSlice({
   name: 'currentTodo',
   initialState,
-  reducers: {},
+  reducers: {
+    saveTodo(state, action: PayloadAction<Todo>) {
+      // eslint-disable-next-line no-param-reassign
+      state.currentTodo = action.payload;
+    },
+
+    saveUser(state, action: PayloadAction<User>) {
+      // eslint-disable-next-line no-param-reassign
+      state.currentUser = action.payload;
+    },
+    clearUser(state) {
+      // eslint-disable-next-line no-param-reassign
+      state.currentTodo = null;
+    },
+
+    clearTodo(state) {
+      // eslint-disable-next-line no-param-reassign
+      state.currentTodo = null;
+    },
+
+    changeIsLoading(state, action: PayloadAction<boolean>) {
+      // eslint-disable-next-line no-param-reassign
+      state.isLoading = action.payload;
+    },
+  },
 });
+
+export const { saveTodo, clearTodo, changeIsLoading, saveUser, clearUser } =
+  currentTodoSlice.actions;
+export default currentTodoSlice.reducer;
--- src/features/filter.ts
+++ src/features/filter.ts
@@ -1,12 +1,36 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { Status } from '../types/Status';
 
-const initialState = {
+export interface FilterStateSlice {
+  query: string;
+  status: Status;
+}
+
+const initialState: FilterStateSlice = {
   query: '',
   status: 'all',
 };
 
 export const filterSlice = createSlice({
-  name: 'filter',
+  name: 'filterSlice',
   initialState,
-  reducers: {},
+  reducers: {
+    setStatus(state, action: PayloadAction<Status>) {
+      // eslint-disable-next-line no-param-reassign
+      state.status = action.payload;
+    },
+
+    setQuery(state, action: PayloadAction<string>) {
+      // eslint-disable-next-line no-param-reassign
+      state.query = action.payload;
+    },
+
+    clearQuery(state) {
+      // eslint-disable-next-line no-param-reassign
+      state.query = '';
+    },
+  },
 });
+
+export const { setStatus, setQuery, clearQuery } = filterSlice.actions;
+export default filterSlice.reducer;
